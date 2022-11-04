6b796ccd215933535d6db6220b7268d2ea693040
Tim Blais <tblais189@gmail.com>
2022-11-04 17:38:01 -0400

    added routes for feed, login, newpost, profile, signup, and index. index and signup routes functional, allowing users to access index page and sign up successfully

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth') 
-const indexController = require('../controllers/index')
+// const signUpController = require('../controllers/signUp')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-router.get('/', indexController.getIndex)
-
+router.get('/', authController.getSignup)
+router.post('/', authController.postSignup)
 
 module.exports = router
