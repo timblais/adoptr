4a60af010e8c019988048e0d5881c01f00da6b46
Tim Blais <tblais189@gmail.com>
2022-11-07 20:02:59 -0500

    added model for comments.

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  postId: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
