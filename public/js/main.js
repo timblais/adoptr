486b92612e257fb3c78331ce54022fe1f68c751b
Tim Blais <tblais189@gmail.com>
2022-11-07 20:41:38 -0500

    initial like button function build in main js.

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+
+
+async function likePost(){
+    const postId = this.parentNode.dataset.id
+
+    try{
+        const response = await fetch('/like', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'postIdFromJSFile': postId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
