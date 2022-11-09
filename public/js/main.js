37a18c13e2d17202eff1fa167a7b4f6f9801822e
Tim Blais <tblais189@gmail.com>
2022-11-08 19:49:39 -0500

    updated main.js. likePost function is working, created button listener for likePost function. Created class-based innerText values for like button in views. Will ultimately update to an icon

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,25 @@
+const likeButton = document.querySelectorAll('.likeButton')
 
+const notLiked = document.querySelectorAll('.notLiked')
+const alreadyLiked = document.querySelectorAll('.liked')
+
+Array.from(notLiked).forEach((el) => {
+    el.innerText = 'Like'
+})
+
+Array.from(alreadyLiked).forEach((el) => {
+    el.innerText = 'Liked!'
+})
+
+Array.from(likeButton).forEach((el) => {
+    el.addEventListener('click', likePost)
+})
 
 async function likePost(){
     const postId = this.parentNode.dataset.id
 
     try{
-        const response = await fetch('/like', {
+        const response = await fetch('../like', {
             method: 'put',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
