d9087c4e8e1cb2ceb6a53f4ac9799f7174cff572
Tim Blais <tblais189@gmail.com>
2022-11-14 19:36:17 -0500

    removed multer routing file requirements

--- routes/newPost.js
+++ routes/newPost.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const upload = require("../middleware/multer");
 const postsController = require('../controllers/posts') 
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', ensureAuth, postsController.getNewPost)
-router.post('/', ensureAuth, upload.single("file"), postsController.createNewPost)
+router.post('/', ensureAuth, postsController.createNewPost)
 
 module.exports = router
